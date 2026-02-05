ec565d82c05efb3f546ffcc26cbc506840053bae
Fan-Yong <64456663+Fan-Yong@users.noreply.github.com>
2026-02-05 14:31:53 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+// ==========================================
+// 📂 资源数据库
+// 这里的修改会即时生效
+// ==========================================
+    
+const config = {
+	// === 写法 1：带说明的配置 (推荐) ===
+	"1": {
+		url: "https://drive.google.com/file/d/1f8XhO1L51YsjxetwSTjHPZ3llKcVKGkc/view?usp=sharing",
+		desc: "这是营养药品1。"
+	},
+
+	"2": {
+		url: "https://drive.google.com/file/d/1bjyuotViTIjwBUX7AaldVpGGhMQXy6a0/view?usp=sharing",
+		desc: "这是营养药品2"
+	},
+
+	"3": {
+		url: "https://drive.google.com/file/d/1zsfj2HNK48BAQ-ZBZyrYf-FcwPmahVSP/view?usp=sharing",
+		desc: "这是营养药品3"
+	},
+
+	"10000": {
+		url: "https://fan-yong.github.io/note/codesample.html",
+		desc: "代码的模板，在GitHub上的note库"
+	},
+	
+	// === 写法 2：如果不写 desc，就直接跳转 (兼容旧写法) ===
+	"101": "https://github.com" 
+};
